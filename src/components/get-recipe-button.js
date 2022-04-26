71681b83879ddf896415d6986cd4338ce92d3262
Ritik Sharma <up930788@myport.ac.uk>
2022-04-26 20:19:09 +0100

    button changes

--- src/components/get-recipe-button.js
+++ src/components/get-recipe-button.js
@@ -26,7 +26,7 @@ export default class GetRecipeButton extends Component {
                 pathname: "/recipe_guide",
                 state: recipe
                 }}>
-                <button class="btn btn-outline-success">Get recipe!</button>
+                <button class="btn btn-outline-success btn-lg" >Get recipe!</button>
                 </Link>
             </div>
         );  
