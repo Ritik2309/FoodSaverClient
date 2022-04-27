953883f88cd718fd5096101915449107153ea934
Ritik Sharma <up930788@myport.ac.uk>
2022-04-28 00:23:36 +0100

    google maps api

--- src/components/google-maps.js
+++ src/components/google-maps.js
@@ -5,7 +5,7 @@ import {Map, InfoWindow, Marker , GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends Component {
   render() {
     return (
-        <Map google={this.props.google} zoom= {10} initialCenter ={{lat:43.071754,lng:-70.762550}}>
+        <Map google={this.props.google} zoom= {10} initialCenter ={{lat:50.79899,lng:-1.09125}}>
             <Marker onClick={this.onMarkerClick} name={'Portsmouth'}/>
             <InfoWindow on CLose={this.onInfoWindowClosr}>
             <div>
