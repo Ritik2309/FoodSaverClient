2005f36c59759b7fa638f71f091fb2db66f3ec46
Ritik Sharma <up930788@myport.ac.uk>
2022-04-28 00:14:47 +0100

    google maps api

--- src/components/google-maps.js
+++ src/components/google-maps.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {Map, InfoWindow, Marker , GoogleApiWrapper} from 'gooogle-maps-react';
+import {Map, InfoWindow, Marker , GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
   render() {
