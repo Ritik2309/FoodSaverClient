5c22bf037192d994122d9ff38e81e74a93640529
Ritik Sharma <up930788@myport.ac.uk>
2022-04-28 00:38:47 +0100

    google maps api

--- src/components/google-maps.js
+++ src/components/google-maps.js
@@ -3,15 +3,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {Map, InfoWindow, Marker , GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+      };
+      onMarkerClick = (props, marker, e) =>
+            this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+            });
+        
+        onMapClicked = (props) => {
+            if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+            }
+        };
+        
+     
   render() {
     return (
-        <Map google={this.props.google} zoom= {10} initialCenter ={{lat:50.79899,lng:-1.09125}}>
-            <Marker onClick={this.onMarkerClick} name={'Portsmouth'}/>
-            <InfoWindow on CLose={this.onInfoWindowClosr}>
+        <Map google={this.props.google}
+            onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
             <div>
-                
+                <h1>{this.state.selectedPlace.name}</h1>
             </div>
-            </InfoWindow>
+        </InfoWindow>
         </Map>
     );
   }
