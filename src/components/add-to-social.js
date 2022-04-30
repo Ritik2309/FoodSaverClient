afb55fb1599921cff2e92393622ee1e4ff067578
Ritik Sharma <up930788@myport.ac.uk>
2022-04-30 17:41:01 +0100

    layout changes and html social page changes

--- src/components/add-to-social.js
+++ src/components/add-to-social.js
@@ -97,11 +97,11 @@ export default class AddToSocial extends Component {
             <div><label htmlFor="exampleFormControlTextarea1" style={{fontWeight: 'bold'}}> <br /> Currently selected image: <img class="float-right" className={styles.recipePreviewSize} src={this.state.imageLink}></img> <label htmlFor="exampleFormControlTextarea1"style={{fontWeight: 'bold'}}>Search for food images to add to post: <label><RecipeImageBar /></label> <br /> </label></label><div>
             
             </div></div>
-            <label htmlFor="exampleFormControlTextarea1" style={{fontWeight: 'bold'}}> <br />currently selected location: </label><br /><label>{this.state.location} <label><br /><Link to={{
+            <label htmlFor="exampleFormControlTextarea1" style={{fontWeight: 'bold'}}> <br />currently selected location: </label><br /><label>{this.state.location}<br /><Link to={{
             pathname: "/maps"
               }}>
           <button type="button" class="mx-1 btn btn-primary float-right">Select New Location</button>
-          </Link></label></label>
+          </Link></label>
             <br />
             <br />
             
