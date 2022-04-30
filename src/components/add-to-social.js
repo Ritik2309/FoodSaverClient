016b825da4599ff71a0b5e456a08900ac2fcc1f8
Ritik Sharma <up930788@myport.ac.uk>
2022-04-30 17:33:52 +0100

    first commit

--- src/components/add-to-social.js
+++ src/components/add-to-social.js
@@ -94,10 +94,10 @@ export default class AddToSocial extends Component {
 
           <form>
             <div class="form-group">
-            <div><label htmlFor="exampleFormControlTextarea1"> <br /> Currently selected image: <img class="float-right" className={styles.recipePreviewSize} src={this.state.imageLink}></img> <label htmlFor="exampleFormControlTextarea1">Search for food images to add to post: <RecipeImageBar /><br /> </label></label><div>
+            <div><label htmlFor="exampleFormControlTextarea1"style={{fontWeight: 'bold'}}> <br /> Currently selected image: <img class="float-right" className={styles.recipePreviewSize} src={this.state.imageLink}></img> <label htmlFor="exampleFormControlTextarea1">Search for food images to add to post: <RecipeImageBar /><br /> </label></label><div>
             
             </div></div>
-            <label htmlFor="exampleFormControlTextarea1" >currently selected location: {this.state.location}  style={{fontWeight: 'bold'}} </label>
+            <label htmlFor="exampleFormControlTextarea1" style={{fontWeight: 'bold'}}> <br />currently selected location:   </label><label>{this.state.location}</label>
             <br />
             <br />
             <label><Link to={{
